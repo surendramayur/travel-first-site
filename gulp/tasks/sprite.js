3ae185f42d565293259b1a363cfd6f519437b5b6
Mayur <surendramayur@gmail.com>
2017-09-01 14:47:00 +0530

    moved sprite img to root edited icons task installed npm rename

--- gulp/tasks/sprite.js
+++ gulp/tasks/sprite.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
-svgSprite = require('gulp-svg-sprite');
+svgSprite = require('gulp-svg-sprite'),
+rename = require('gulp-rename');
 
 var config = {
 	mode: {
 		css: {
+			sprite: 'sprite.svg',
 			render: {
 				css: {
 					template: './gulp/templates/sprite.css'
@@ -22,3 +24,18 @@ gulp.task('createSprite', function(){
 	
 	
 });
+
+gulp.task('copySpriteGraphic', ['createSprite'], function(){
+	return gulp.src('./app/temp/sprite/css/**/*.svg')
+	.pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+
+gulp.task('copySpriteCSS', ['createSprite'], function(){
+	return gulp.src('./app/temp/sprite/css/*.css')
+	.pipe(rename('_sprite.css'))
+	.pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
+
