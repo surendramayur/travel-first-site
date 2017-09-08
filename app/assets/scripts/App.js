3516fffdffe7593ba2efb9cf3b79f54146265aec
Mayur <surendramayur@gmail.com>
2017-09-08 11:09:01 +0530

    added mobilemenujs to make navigation visible/invisible on click on mobile view modifier class and organised javascript used

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,17 +1,3 @@
-var $ = require('jquery');
-//var Person = require('./modules/Person');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-	payTaxes(){
-		console.log(this.name + " is now Adulting and paying taxes and stuff.");
-	}
-}
-
-var john = new Person("John Doe", "Blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "Orange");
-jane.greet();
-jane.payTaxes();
-
+var mobileMenu = new MobileMenu();
